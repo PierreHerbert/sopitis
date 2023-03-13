b5ec8622358f51d7e90bdc033e692d7f2941cf60
mrideau <matis.rideau@hotmail.com>
2023-03-13 16:33:38 +0100

    feat: Fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,9 +13,9 @@
         {{ config('app.name') }}
     </title>
 
-    {{--
-        TODO: Add fonts
-    --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;700&family=Work+Sans:wght@300;400;700&display=swap" rel="stylesheet">
 
     @vite('resources/sass/app.scss')
 
