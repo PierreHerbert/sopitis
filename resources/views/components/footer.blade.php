b013f3cc8edf53098aa003aef8b6d456024f1035
mrideau <matis.rideau@hotmail.com>
2023-03-14 16:11:45 +0100

    feat: Footer info

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,12 +1,12 @@
 <footer class="bg-black text-neutral-400">
     <div class="container divide-y">
-        <div class="py-5 lex">
+        <div class="py-10 lex">
             <div>
-                <p class="font-bold text-white">Title</p>
-                <p>Lorem</p>
+                <p class="font-bold text-white text-lg">Sopitis</p>
+                <p class="text-sm">Pour toutes demandes supplémentaires: <a href="mailto:contact@sopitis.fr" class="text-primary">contact@sopitis.fr</a></p>
             </div>
         </div>
-        <div class="py-5 text-sm">
+        <div class="py-10 text-sm">
             <p>© 2023 Sopitis - Traveling Agency</p>
         </div>
     </div>
