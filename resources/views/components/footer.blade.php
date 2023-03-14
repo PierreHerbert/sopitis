fb3054798f5059dfb90ff6c88a9e50f8ac97af73
mrideau <matis.rideau@hotmail.com>
2023-03-14 10:30:44 +0100

    feat: Init footer component

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,13 @@
+<footer class="bg-black text-neutral-400">
+    <div class="container divide-y">
+        <div class="py-5 lex">
+            <div>
+                <p class="font-bold text-white">Title</p>
+                <p>Lorem</p>
+            </div>
+        </div>
+        <div class="py-5 text-sm">
+            <p>© 2023 Sopitis - Traveling Agency</p>
+        </div>
+    </div>
+</footer>
